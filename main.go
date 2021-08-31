b211c4853f9f746a147abc645417ee841fc5c696
DavidSpek <vanderspek.david@gmail.com>
2021-08-31 15:19:06 +0200

    fix outpus

--- main.go
+++ main.go
@@ -127,11 +127,11 @@ func main() {
 	newPatch.Set(latestTag.String())
 	newPatch.BumpPatch()
 
-	fmt.Println(fmt.Sprintf(`::set-output name=latest_tag::%s`, latestTag))
-	fmt.Println(fmt.Sprintf(`::set-output name=new_major=::%s`, newMajor))
-	fmt.Println(fmt.Sprintf(`::set-output name=new_v_major=::v%s`, newMajor))
-	fmt.Println(fmt.Sprintf(`::set-output name=new_minor=::%s`, newMinor))
-	fmt.Println(fmt.Sprintf(`::set-output name=new_v_minor=::v%s`, newMinor))
-	fmt.Println(fmt.Sprintf(`::set-output name=new_patch=::%s`, newPatch))
-	fmt.Println(fmt.Sprintf(`::set-output name=new_v_patch=::v%s`, newPatch))
+	fmt.Println(fmt.Sprintf(`::set-output name=latest_tag::%s ::set-output name=new_major=::%s ::set-output name=new_minor=::%s ::set-output name=new_patch=::%s ::set-output name=new_v_major=::v%s ::set-output name=new_v_minor=::v%s ::set-output name=new_v_patch=::v%s`, latestTag, newMajor, newMinor, newPatch, newMajor, newMinor, newPatch))
+	// fmt.Println(fmt.Sprintf(`::set-output name=new_major=::%s`, newMajor))
+	// fmt.Println(fmt.Sprintf(`::set-output name=new_v_major=::v%s`, newMajor))
+	// fmt.Println(fmt.Sprintf(`::set-output name=new_minor=::%s`, newMinor))
+	// fmt.Println(fmt.Sprintf(`::set-output name=new_v_minor=::v%s`, newMinor))
+	// fmt.Println(fmt.Sprintf(`::set-output name=new_patch=::%s`, newPatch))
+	// fmt.Println(fmt.Sprintf(`::set-output name=new_v_patch=::v%s`, newPatch))
 }
